81a828f4682776182565e98fc9d8f6aa5b93084a
Maria Fernanda <maria.ibanez.leiva@gmail.com>
2022-02-27 17:45:16 -0300

    Change Server sr to client again

--- pages/clients.js
+++ pages/clients.js
@@ -2,21 +2,21 @@ import { useEffect, useState } from 'react';
 import GridClients from '../components/GridClients';
 import Layout from '../components/Layout';
 
-export default function Clients({data}) {
+export default function Clients() {
 
-  // const [data, setData] = useState([]);
-  // const url = 'https://www.ag-grid.com/example-assets/olympic-winners.json';
+  const [data, setData] = useState([]);
+  const url = 'https://www.ag-grid.com/example-assets/olympic-winners.json';
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const response = await fetch(url);
-  //     const result = await response.json();
-  //     setData(result);
-  //   }
-  //   fetchData();
-  // }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(url);
+      const result = await response.json();
+      setData(result);
+    }
+    fetchData();
+  }, []);
 
-  // console.log(data)
+  console.log(data)
   
   return (
     <Layout
@@ -28,16 +28,16 @@ export default function Clients({data}) {
   )
 }
 
-export async function getServerSideProps(context) {
-  const develop = process.env.NODE_ENV ?  'http://localhost:3000' : 'https://';
+// export async function getServerSideProps(context) {
+//   const develop = process.env.NODE_ENV ?  'http://localhost:3000' : 'https://';
 
-  console.log(develop)
-  // const res = await fetch(`${develop}/api/client`);
-  const res = await fetch('https://www.ag-grid.com/example-assets/olympic-winners.json')
-  const data = await res.json();
-  return{
-    props: {
-      data
-    }
-  }
-}
+//   console.log(develop)
+//   // const res = await fetch(`${develop}/api/client`);
+//   const res = await fetch('https://www.ag-grid.com/example-assets/olympic-winners.json')
+//   const data = await res.json();
+//   return{
+//     props: {
+//       data
+//     }
+//   }
+// }
